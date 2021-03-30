d96f40ad9c767181b60e64aa1268083857653130
Dilan Ertoktaş <dilanertoktas@hotmail.com>
2021-03-30 21:54:23 +0300

    template

---
+++ Face Recognition/Controllers/KameralarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Face_Recognition.Controllers
+{
+    public class KameralarController : Controller
+    {
+        // GET: Kameralar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
